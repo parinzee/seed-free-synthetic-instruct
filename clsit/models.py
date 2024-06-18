22a023aa727790d3ebfece595c8f7b92bd596e93
Parinthapat Pengpun <parinzee@protonmail.com>
2024-06-18 11:16:59 +0000

    Models: Add Ollama Support

--- clsit/models.py
+++ clsit/models.py
@@ -1,4 +1,6 @@
 import anthropic
+import ollama
+
 from clsit.config import settings
 
 import pycountry
@@ -29,11 +31,40 @@ def _get_language_name(language_code):
     except KeyError:
         raise ValueError(f"Invalid language code: {language_code}")
 
+class OllamaWrapper:
+    def __init__(
+        self,
+        model_name="llama3:70b-instruct",
+        host_url=None,
+    ):
+        self.client = ollama.Client(
+            host=host_url
+        )
+        self.model_name = model_name
+
+    def generate(self, messages, system, **kwargs):
+        # Add the system prompt to the messages
+        messages = [
+            {"role": "system", "content": system},
+            *messages,
+        ]
+
+        response = self.client.chat(
+            model=self.model_name, messages=messages, stream=False, options=kwargs
+        )
+
+        return response["message"]["content"], response
+
 def get_model_wrapper():
     if settings.model.anthropic.use:
         model_wrapper = ClaudeWrapper(
             model_name=settings.model.anthropic.model,
             api_key=settings.model.anthropic.api_key,
+        )
+    elif settings.model.ollama.use:
+        model_wrapper = OllamaWrapper(
+            model_name=settings.model.ollama.model,
+            host_url=settings.model.ollama.host_url,
         )
     else:
         raise NotImplementedError("Only Anthropic models are supported at the moment.")
@@ -46,7 +77,15 @@ def get_system_prompt():
 
     if settings.culture.enabled:
         system_prompt += settings.culture.prompt
+
+    if not settings.general.custom_system_prompt:
+        system_prompt += " Ensure that ALL of your response and output, other than pre-specified format keys, is in the {language} language. Always output in {language} even when input is in other languages."
+        system_prompt.format(language=_get_language_name(language_code))
+    else:
+        system_prompt += " " + settings.general.custom_system_prompt
+        system_prompt.format(language=_get_language_name(language_code))
     
-    system_prompt += f" Ensure that ALL of your output, other than pre-specified format keys, is in the {_get_language_name(language_code)} language."
+    # system_prompt += f" Ensure that ALL of your response and output, other than pre-specified format keys, is in the {_get_language_name(language_code)} language. Always output in {_get_language_name(language_code)} even when input is in other languages."
+    # system_prompt += f""" You are a helpful, respectful and honest assistant. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If you don’t know the answer to a question, please don’t share false information. please answer the question in Thai."""
 
     return system_prompt
